6908bbd10a99b6624b6523a8fbbb02c65799e80c
peachey <>
2005-06-03 21:26:56 +0000

    Add EphComputer class, which uses EphChooser, TimingModel and containers of ephemerides to perform operations on event-by-event basis.

---
+++ src/EphComputer.cxx
@@ -0,0 +1,63 @@
+/** \file EphComputer.cxx
+    \brief Implementation for EphComputer class.
+    \authors Masaharu Hirayama, GSSC,
+             James Peachey, HEASARC/GSSC
+*/
+#include "pulsarDb/EphChooser.h"
+#include "pulsarDb/EphComputer.h"
+#include "pulsarDb/PulsarDb.h"
+#include "pulsarDb/TimingModel.h"
+
+namespace pulsarDb {
+
+  EphComputer::EphComputer(TimingModel * model, EphChooser * chooser): m_pulsar_eph_cont(), m_orbital_eph_cont(),
+    m_model(model), m_chooser(chooser) {
+  }
+
+  void EphComputer::load(const PulsarDb & pulsar_db) {
+    pulsar_db.getEph(m_pulsar_eph_cont);
+    pulsar_db.getEph(m_orbital_eph_cont);
+  }
+
+  FrequencyEph EphComputer::calcPulsarEph(const AbsoluteTime & ev_time) const {
+    const PulsarEph & eph(m_chooser->choose(m_pulsar_eph_cont, ev_time));
+    return m_model->calcEphemeris(eph, ev_time);
+  }
+
+  void EphComputer::cancelPdot(AbsoluteTime & ev_time) const {
+    const PulsarEph & eph(m_chooser->choose(m_pulsar_eph_cont, ev_time));
+    m_model->correctPdot(eph, ev_time);
+  }
+
+  double EphComputer::calcPulsePhase(const AbsoluteTime & ev_time) const {
+    const PulsarEph & eph(m_chooser->choose(m_pulsar_eph_cont, ev_time));
+    return m_model->calcPhase(eph, ev_time);
+  }
+
+  void EphComputer::modulateBinary(AbsoluteTime & emission_time) const {
+    const OrbitalEph & eph(m_chooser->choose(m_orbital_eph_cont, emission_time));
+    m_model->modulateBinary(eph, emission_time);
+  }
+
+  void EphComputer::demodulateBinary(AbsoluteTime & arrival_time) const {
+    const OrbitalEph & eph(m_chooser->choose(m_orbital_eph_cont, arrival_time));
+    m_model->demodulateBinary(eph, arrival_time);
+  }
+
+  PulsarEphCont & EphComputer::getPulsarEphCont() {
+    return m_pulsar_eph_cont;
+  }
+
+  const PulsarEphCont & EphComputer::getPulsarEphCont() const {
+    return m_pulsar_eph_cont;
+  }
+
+  OrbitalEphCont & EphComputer::getOrbitalEphCont() {
+    return m_orbital_eph_cont;
+  }
+
+  const OrbitalEphCont & EphComputer::getOrbitalEphCont() const {
+    return m_orbital_eph_cont;
+  }
+
+}
