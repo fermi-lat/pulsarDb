4062a1dd95d4b3e81a5a5e6ca896177dba1cfefd
peachey <>
2005-08-12 19:26:42 +0000

    Correct email addresses.

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,8 +1,8 @@
 /**
     \mainpage pulsarDb package
 
-    \author  Masaharu Hirayama hirayama@lheamail.gsfc.nasa.gov
-             James Peachey peachey@lheamail.gsfc.nasa.gov
+    \author  Masaharu Hirayama hirayama@jca.umbc.edu
+             James Peachey peachey@milkyway.gsfc.nasa.gov
 
     \section intro Introduction
     The purpose of this package is to provide an interface to the standard pulsar ephemeris
