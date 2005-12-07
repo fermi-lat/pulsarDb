614b7f45138399ba9ff07525a080e6e7ed74f348
peachey <>
2005-12-07 21:10:55 +0000

    Represent time with 1 double value for days, another for seconds, instead of one long double for either days or seconds.

--- pulsarDb/Duration.h
+++ pulsarDb/Duration.h
@@ -30,7 +30,15 @@ namespace pulsarDb {
           \param t The amount of time.
           \param unit The unit in which time t is measured.
       */
-      Duration(long double t, TimeUnit_e unit): m_t(t), m_unit(unit) {}
+      Duration(long double t, TimeUnit_e unit): m_day(), m_sec() {
+        if (unit == UnitDay) {
+          m_day = std::floor(t);
+          m_sec = (t - m_day) * SecPerDay();
+        } else if (unit == UnitSec) {
+          m_day = std::floor(t * DayPerSec());
+          m_sec = t - (m_day * SecPerDay());
+        }
+      }
 
       /// \brief Return the current value of this time in days.
       long double day() const;
@@ -39,6 +47,10 @@ namespace pulsarDb {
       long double sec() const;
 
       Duration op(long double (*func)(long double)) const;
+
+      Duration operator +(const Duration & dur) const { Duration result = *this; result += dur; return result; }
+
+      Duration operator +=(const Duration & dur) { m_day += dur.m_day; m_sec += dur.m_sec; return *this; }
 
       Duration & operator -=(const Duration & dur);
 
@@ -54,41 +66,23 @@ namespace pulsarDb {
       void write(std::ostream & os) const;
 
     private:
-      long double m_t;
-      TimeUnit_e m_unit;
+      long double m_day;
+      long double m_sec;
   };
 
   inline long double Duration::day() const {
-    switch (m_unit) {
-      case UnitDay:
-        break; // No modifcation necessary.
-      case UnitSec:
-        return m_t * DayPerSec();
-    }
-    return m_t;
+    return m_day + m_sec * DayPerSec();
   }
 
   inline long double Duration::sec() const {
-    switch (m_unit) {
-      case UnitDay:
-        return m_t * SecPerDay();
-      case UnitSec:
-        break; // No modifcation necessary.
-    }
-    return m_t;
+    return m_sec + m_day * SecPerDay();
   }
 
-  inline Duration Duration::op(long double (*func)(long double)) const { return Duration(func(m_t), m_unit); }
+  inline Duration Duration::op(long double (*func)(long double)) const { return Duration(func(day()), UnitDay); }
 
   inline Duration & Duration::operator -=(const Duration & dur) {
-    switch (m_unit) {
-      case UnitDay:
-        m_t -= dur.day();
-        break;
-      case UnitSec:
-        m_t -= dur.sec();
-        break;
-    }
+    m_day -= dur.m_day;
+    m_sec -= dur.m_sec;
     return *this;
   }
 
@@ -99,8 +93,9 @@ namespace pulsarDb {
   }
 
   inline void Duration::write(std::ostream & os) const {
-    os << m_t << (m_unit == UnitDay ? " day" : " second");
-    if (m_t == 1.) os << "s";
+    double dday = day();
+    os << dday << " day";
+    if (dday == 1.) os << "s";
   }
 
   inline std::ostream & operator <<(std::ostream & os, const Duration & dur) {
