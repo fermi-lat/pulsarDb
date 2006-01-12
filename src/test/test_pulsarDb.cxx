3902ed816dc7be62b095f0683f8dc77ffaa6424b
peachey <>
2006-01-12 20:41:15 +0000

    Fix test message and remove commented out code.

--- src/test/test_pulsarDb.cxx
+++ src/test/test_pulsarDb.cxx
@@ -420,10 +420,9 @@ void PulsarDbTest::testAbsoluteTime() {
     ErrorMsg(method_name) << "after TaiTime tai(tt), tai.getMjd() returned " << tai.getMjd() << ", not " << expected <<
       ", as expected" << std::endl;
 
-//  tai = TaiTime(Duration(0, -32.184));
   tai = TaiTime(-tt.getMjd());
   if (-tt.getMjd() != tai.getMjd())
-    ErrorMsg(method_name) << "after tai = TaiTime(Duration(0, -32.184)), -tt.getMjd() is " << -tt.getMjd() <<
+    ErrorMsg(method_name) << "after tai = TaiTime(-tt.getMjd()), -tt.getMjd() is " << -tt.getMjd() <<
       " and tai.getMjd() is " << tai.getMjd() << std::endl;
 
   tt = tai;
