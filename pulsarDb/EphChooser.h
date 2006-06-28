c038d9b70ae1553fd3610bd82cf3c2d37e639068
peachey <>
2006-06-28 19:43:54 +0000

    Use tolerance to avoid checking equality of floating point values, and refactor the logic of the choosers to make them consistent with this.

--- pulsarDb/EphChooser.h
+++ pulsarDb/EphChooser.h
@@ -9,6 +9,9 @@
 #include "pulsarDb/OrbitalEph.h"
 #include "pulsarDb/PulsarEph.h"
 
+#include "timeSystem/Duration.h"
+#include "timeSystem/ElapsedTime.h"
+
 namespace timeSystem {
   class AbsoluteTime;
 }
@@ -31,6 +34,72 @@ namespace pulsarDb {
                  which appears last in the table is selected.
           \param t The time of interest.
       */
+      virtual const PulsarEph & choose(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const = 0;
+
+      /** \brief Choose the best ephemeris for the given absolute time. Throws an exception if no ephemeris is found.
+
+                 The ephmeris returned contains the time in the half-open interval [VALID_SINCE, VALID_UNTIL)
+                 If more than one candidate ephemeris contains the time, the ephemeris with the latest start
+                 time is chosen. If more than one candidate has the same start time, the one with the latest
+                 stop time is chosen. If more than one candidate has the same start and stop times, the ephemeris
+                 which appears last in the table is selected.
+          \param t The time of interest.
+      */
+      virtual const OrbitalEph & choose(const OrbitalEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const = 0;
+
+      virtual EphChooser * clone() const = 0;
+
+      const PulsarEph & findClosest(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const;
+
+      const OrbitalEph & findClosest(const OrbitalEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const;
+  };
+
+  /** \class StrictEphChooser
+      \brief Abstraction providing method for choosing best ephemeris from a container of candidate.
+  */
+  class StrictEphChooser : public EphChooser {
+    public:
+      StrictEphChooser(const timeSystem::ElapsedTime & tolerance = timeSystem::ElapsedTime("TDB", timeSystem::Duration(0, 1.e-6)));
+
+      /** \brief Choose the best ephemeris for the given absolute time. Throws an exception if no ephemeris is found.
+
+                 The ephmeris returned contains the time in the half-open interval [VALID_SINCE, VALID_UNTIL)
+                 If more than one candidate ephemeris contains the time, the ephemeris with the latest start
+                 time is chosen. If more than one candidate has the same start time, the one with the latest
+                 stop time is chosen. If more than one candidate has the same start and stop times, the ephemeris
+                 which appears last in the table is selected.
+          \param t The time of interest.
+      */
+      virtual const PulsarEph & choose(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const;
+
+      virtual const OrbitalEph & choose(const OrbitalEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const;
+
+      virtual EphChooser * clone() const;
+
+    protected:
+      timeSystem::ElapsedTime m_tolerance;
+  };
+
+  /** \class SloppyEphChooser
+      \brief Abstraction providing method for choosing best ephemeris from a container of candidate, being
+      less pedantic than EphChooser.
+  */
+  class SloppyEphChooser : public EphChooser {
+    public:
+      SloppyEphChooser();
+
+      SloppyEphChooser(const timeSystem::ElapsedTime & tolerance);
+
+      /** \brief Choose the ephemeris closest to the given time even if the time is outside its interval of validity.
+                 If no ephemeris meets these requirements, an exception is thrown.
+
+                 The ephmeris returned contains the time in the half-open interval [VALID_SINCE, VALID_UNTIL)
+                 If more than one candidate ephemeris contains the time, the ephemeris with the latest start
+                 time is chosen. If more than one candidate has the same start time, the one with the latest
+                 stop time is chosen. If more than one candidate has the same start and stop times, the ephemeris
+                 which appears last in the table is selected.
+          \param t The time of interest.
+      */
       virtual const PulsarEph & choose(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const;
 
       /** \brief Choose the best ephemeris for the given absolute time. Throws an exception if no ephemeris is found.
@@ -45,27 +114,9 @@ namespace pulsarDb {
       virtual const OrbitalEph & choose(const OrbitalEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const;
 
       virtual EphChooser * clone() const;
-  };
 
-  /** \class SloppyEphChooser
-      \brief Abstraction providing method for choosing best ephemeris from a container of candidate, being
-      less pedantic than EphChooser.
-  */
-  class SloppyEphChooser : public EphChooser {
-    public:
-      /** \brief Choose the ephemeris closest to the given time even if the time is outside its interval of validity.
-                 If no ephemeris meets these requirements, an exception is thrown.
-
-                 The ephmeris returned contains the time in the half-open interval [VALID_SINCE, VALID_UNTIL)
-                 If more than one candidate ephemeris contains the time, the ephemeris with the latest start
-                 time is chosen. If more than one candidate has the same start time, the one with the latest
-                 stop time is chosen. If more than one candidate has the same start and stop times, the ephemeris
-                 which appears last in the table is selected.
-          \param t The time of interest.
-      */
-      virtual const PulsarEph & choose(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const;
-
-      virtual EphChooser * clone() const;
+    protected:
+      StrictEphChooser m_strict_chooser;
   };
 
 }
--- src/EphChooser.cxx
+++ src/EphChooser.cxx
@@ -11,7 +11,6 @@
 
 #include "timeSystem/AbsoluteTime.h"
 #include "timeSystem/Duration.h"
-#include "timeSystem/ElapsedTime.h"
 #include "timeSystem/IntFracPair.h"
 #include "timeSystem/TimeInterval.h"
 
@@ -19,82 +18,9 @@ using namespace timeSystem;
 
 namespace pulsarDb {
 
-  const PulsarEph & EphChooser::choose(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
-    PulsarEphCont::const_iterator candidate = ephemerides.end();
-
-    for (PulsarEphCont::const_iterator itor = ephemerides.begin(); itor != ephemerides.end(); ++itor) {
-      // See if this ephemeris contains the time.
-      if ((*itor)->valid_since() <= t && t < (*itor)->valid_until()) {
-
-        // See if this is the first candidate, which is automatically accepted.
-        if (ephemerides.end() == candidate) {
-          candidate = itor;
-        // Otherwise, prefer the eph which starts later.
-        } else if ((*itor)->valid_since() > (*candidate)->valid_since()) {
-          candidate = itor;
-// TODO: Re-consider how to implement "==" with new AbsoluteTime, etc.
-//        } else if ((*itor)->valid_since() == (*candidate)->valid_since()) {
-        } else if ((*itor)->valid_since() >= (*candidate)->valid_since()) {
-          // The two start at the same time, so break the tie based on which one is valid longer.
-          // Note that in a tie here, the one selected is the one appearing last in the sequence.
-          if ((*itor)->valid_until() >= (*candidate)->valid_until())
-            candidate = itor;
-        }
-      }
-    }
-
-    // If no candidate was found, throw an exception.
-    if (ephemerides.end() == candidate) {
-      std::ostringstream os;
-      os << "EphChooser::choose could not find a spin ephemeris for time " << t;
-      throw std::runtime_error(os.str());
-    }
-
-    return *(*candidate);
-  }
-
-  const OrbitalEph & EphChooser::choose(const OrbitalEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
-    if (ephemerides.empty()) throw std::runtime_error("EphChooser::choose was passed empty container of ephemerides");
-    // Start with minimum = maximum value.
-    double min_time_diff = std::numeric_limits<double>::max();
-
-    OrbitalEphCont::const_iterator candidate = ephemerides.end();
-    
-    // Find the closest ephemeris time to the given time.
-    for (OrbitalEphCont::const_iterator itor = ephemerides.begin(); itor != ephemerides.end(); ++itor) {
-//      IntFracPair diff_pair = ((*itor)->t0() - t).computeElapsedTime("TDB").getTime().getValue(Sec);
-//      double time_diff = std::fabs(diff_pair.getIntegerPart() + diff_pair.getFractionalPart());
-      double time_diff = std::fabs((*itor)->dt(t));
-      if (time_diff < min_time_diff) {
-        candidate = itor;
-        min_time_diff = time_diff;
-      }
-    }
-
-    // If no candidate was found, throw an exception.
-    if (ephemerides.end() == candidate) {
-      std::ostringstream os;
-      os << "EphChooser::choose could not find an orbital ephemeris for time " << t;
-      throw std::runtime_error(os.str());
-    }
-
-    return *(*candidate);
-  }
-
-  EphChooser * EphChooser::clone() const {
-    return new EphChooser(*this);
-  }
-
-  const PulsarEph & SloppyEphChooser::choose(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
-    if (ephemerides.empty()) throw std::runtime_error("SloppyEphChooser::choose was passed empty container of ephemerides");
+  const PulsarEph & EphChooser::findClosest(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
+    if (ephemerides.empty()) throw std::runtime_error("EphChooser::findClosest was passed empty container of ephemerides");
     using std::fabs;
-
-    // First try to get a strictly correct choice.
-    try {
-      return EphChooser::choose(ephemerides, t);
-    } catch (const std::exception &) {
-      // Ignore this exception because the sloppy code below will then try.
-    }
 
     PulsarEphCont::const_iterator candidate = ephemerides.begin();
 
@@ -124,11 +50,104 @@ namespace pulsarDb {
     // If no candidate was found, throw an exception.
     if (ephemerides.end() == candidate) {
       std::ostringstream os;
-      os << "SloppyEphChooser::choose could not find an ephemeris for time " << t;
+      os << "EphChooser::findClosest could not find an ephemeris for time " << t;
       throw std::runtime_error(os.str());
     }
 
     return *(*candidate);
+  }
+
+  const OrbitalEph & EphChooser::findClosest(const OrbitalEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
+    if (ephemerides.empty()) throw std::runtime_error("EphChooser::findClosest was passed empty container of ephemerides");
+    // Start with minimum = maximum value.
+    double min_time_diff = std::numeric_limits<double>::max();
+
+    OrbitalEphCont::const_iterator candidate = ephemerides.end();
+    
+    // Find the closest ephemeris time to the given time.
+    for (OrbitalEphCont::const_iterator itor = ephemerides.begin(); itor != ephemerides.end(); ++itor) {
+//      IntFracPair diff_pair = ((*itor)->t0() - t).computeElapsedTime("TDB").getTime().getValue(Sec);
+//      double time_diff = std::fabs(diff_pair.getIntegerPart() + diff_pair.getFractionalPart());
+      double time_diff = std::fabs((*itor)->dt(t));
+      if (time_diff < min_time_diff) {
+        candidate = itor;
+        min_time_diff = time_diff;
+      }
+    }
+
+    // If no candidate was found, throw an exception.
+    if (ephemerides.end() == candidate) {
+      std::ostringstream os;
+      os << "EphChooser::findClosest could not find an orbital ephemeris for time " << t;
+      throw std::runtime_error(os.str());
+    }
+
+    return *(*candidate);
+  }
+
+  StrictEphChooser::StrictEphChooser(const timeSystem::ElapsedTime & tolerance): m_tolerance(tolerance) {}
+
+  const PulsarEph & StrictEphChooser::choose(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
+    PulsarEphCont::const_iterator candidate = ephemerides.end();
+
+    for (PulsarEphCont::const_iterator itor = ephemerides.begin(); itor != ephemerides.end(); ++itor) {
+      // See if this ephemeris contains the time.
+      if ((*itor)->valid_since() <= t && t < (*itor)->valid_until()) {
+
+        // See if this is the first candidate, which is automatically accepted.
+        if (ephemerides.end() == candidate) {
+          candidate = itor;
+        } else if ((*itor)->valid_since().equivalentTo((*candidate)->valid_since(), m_tolerance)) {
+          // The two start at the same time, so break the tie based on which one is valid longer.
+          // Note that in a tie here, the one selected is the one appearing last in the sequence.
+          if ((*itor)->valid_until() > (*candidate)->valid_until() ||
+            (*itor)->valid_until().equivalentTo((*candidate)->valid_until(), m_tolerance))
+            candidate = itor;
+        // Otherwise, prefer the eph which starts later.
+        } else if ((*itor)->valid_since() > (*candidate)->valid_since()) {
+          candidate = itor;
+        }
+      }
+    }
+
+    // If no candidate was found, throw an exception.
+    if (ephemerides.end() == candidate) {
+      std::ostringstream os;
+      os << "StrictEphChooser::choose could not find a spin ephemeris for time " << t;
+      throw std::runtime_error(os.str());
+    }
+
+    return *(*candidate);
+  }
+
+  const OrbitalEph & StrictEphChooser::choose(const OrbitalEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
+    return findClosest(ephemerides, t);
+  }
+
+  EphChooser * StrictEphChooser::clone() const {
+    return new StrictEphChooser(*this);
+  }
+
+  SloppyEphChooser::SloppyEphChooser(): m_strict_chooser() {}
+
+  SloppyEphChooser::SloppyEphChooser(const timeSystem::ElapsedTime & tolerance): m_strict_chooser(tolerance) {}
+
+  const PulsarEph & SloppyEphChooser::choose(const PulsarEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
+    if (ephemerides.empty()) throw std::runtime_error("SloppyEphChooser::choose was passed empty container of ephemerides");
+    using std::fabs;
+
+    // First try to get a strictly correct choice.
+    try {
+      return m_strict_chooser.choose(ephemerides, t);
+    } catch (const std::exception &) {
+      // Ignore this exception because the sloppy code below will then try.
+    }
+
+    return findClosest(ephemerides, t);
+  }
+
+  const OrbitalEph & SloppyEphChooser::choose(const OrbitalEphCont & ephemerides, const timeSystem::AbsoluteTime & t) const {
+    return findClosest(ephemerides, t);
   }
 
   EphChooser * SloppyEphChooser::clone() const {
