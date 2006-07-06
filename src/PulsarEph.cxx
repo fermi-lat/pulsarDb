c8f04621459ab2b8d8ee2a9019c90def3fdc1c62
peachey <>
2006-07-06 20:39:22 +0000

    Change output shift operator to take advantage of TimeRep's nice formatting.

--- src/PulsarEph.cxx
+++ src/PulsarEph.cxx
@@ -12,6 +12,7 @@
 #include "timeSystem/ElapsedTime.h"
 #include "timeSystem/IntFracPair.h"
 #include "timeSystem/TimeInterval.h"
+#include "timeSystem/TimeRep.h"
 
 using namespace timeSystem;
 
@@ -27,12 +28,21 @@ namespace pulsarDb {
     int orig_prec = os.precision(15);
     os << std::right;
     os.prefix().width(14);
+    std::string time_system_name = eph.getSystem().getName();
+    MjdRep mjd_rep(time_system_name, 0, 0.);
+
     // Note: below, break into two consecutive strings so that width applies to first part only.
-    if (eph.valid_since() < eph.valid_until())
-      os << "Validity : " << "in range " << "[" << eph.valid_since() << ", " << eph.valid_until() << ")" << std::endl;
-    else
-      os << "Validity : " << "only at epoch" << std::endl;
-    os.prefix().width(14); os << "Epoch = " << eph.epoch() << std::endl;
+    if (!eph.valid_since().equivalentTo(eph.valid_until(), ElapsedTime(time_system_name, Duration(0, 1.e-9)))) {
+      eph.valid_since().getTime(mjd_rep);
+      os << "Validity : " << "in range " << "[" << mjd_rep << ", ";
+      eph.valid_until().getTime(mjd_rep);
+      os << mjd_rep << ")" << std::endl;
+    } else {
+      eph.valid_since().getTime(mjd_rep);
+      os << "Validity : " << "only at time " << mjd_rep << std::endl;
+    }
+    eph.epoch().getTime(mjd_rep);
+    os.prefix().width(14); os << "Epoch = " << mjd_rep << std::endl;
     os.prefix().width(14); os << "Phi0 = " << eph.phi0() << std::endl;
     os.prefix().width(14); os << "F0 = " << eph.f0() << std::endl;
     os.prefix().width(14); os << "F1 = " << eph.f1() << std::endl;
