4bf8bc76c11dcd26b73ba10f3ff42f745c26c560
peachey <>
2007-06-13 20:31:26 +0000

    Renamed to gtephem.

--- src/gtephcomp/gtephcomp.cxx
+++
@@ -1,9 +0,0 @@
-/** \file gtephcomp.cxx
-    \brief Factory for gtephcomp application.
-    \authors Masaharu Hirayama, GSSC,
-             James Peachey, HEASARC/GSSC
-*/
-#include "st_app/StAppFactory.h"
-#include "pulsarDb/EphComputerApp.h"
-
-st_app::StAppFactory<pulsarDb::EphComputerApp> g_factory("gtephcomp");
