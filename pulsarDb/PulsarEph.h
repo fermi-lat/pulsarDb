fffb4fc38badc7a6b2c97770730784d823aada65
hirayama <>
2008-02-11 21:46:10 +0000

    Move all data members of PulsarEph class to derived classes. Remove unnecessary copy constructors of derived classes.

--- pulsarDb/PulsarEph.h
+++ pulsarDb/PulsarEph.h
@@ -24,17 +24,12 @@ namespace pulsarDb {
   */
   class PulsarEph {
     public:
-      PulsarEph(const std::string & time_system_name, const timeSystem::AbsoluteTime & valid_since,
-        const timeSystem::AbsoluteTime & valid_until, const timeSystem::AbsoluteTime & epoch):
-        m_system(&timeSystem::TimeSystem::getSystem(time_system_name)),
-        m_since(valid_since), m_until(valid_until), m_epoch(epoch), m_unit_time(0, 1.) {}
-
       virtual ~PulsarEph() {}
 
-      virtual const timeSystem::TimeSystem & getSystem() const { return *m_system; }
-      virtual const timeSystem::AbsoluteTime & getValidSince() const { return m_since; }
-      virtual const timeSystem::AbsoluteTime & getValidUntil() const { return m_until; }
-      virtual const timeSystem::AbsoluteTime & getEpoch() const { return m_epoch; }
+      virtual const timeSystem::TimeSystem & getSystem() const = 0;
+      virtual const timeSystem::AbsoluteTime & getValidSince() const = 0;
+      virtual const timeSystem::AbsoluteTime & getValidUntil() const = 0;
+      virtual const timeSystem::AbsoluteTime & getEpoch() const = 0;
 
       /** \brief Compute the spin phase of the given time.
                  Note: validity of the ephemeris (valid since and valid until) are not checked.
@@ -69,9 +64,7 @@ namespace pulsarDb {
       virtual st_stream::OStream & write(st_stream::OStream & os) const = 0;
 
     protected:
-      // TODO: Rename dt method to calcElapsedSecond? Or computeElapsedSecond like in PulsarToolApp?
-      virtual double dt(const timeSystem::AbsoluteTime & at1, const timeSystem::AbsoluteTime & at2) const;
-      virtual double dt(const timeSystem::AbsoluteTime & at) const { return dt(at, m_epoch); }
+      PulsarEph() {};
 
       /** \brief Compute the number of revolutions that the pulsar will make between the ephemeris epoch and the given time,
                  including a fractional part of it.
@@ -79,14 +72,6 @@ namespace pulsarDb {
           \param ev_time Time of the event.
       */
       virtual double calcCycleCount(const timeSystem::AbsoluteTime & ev_time) const = 0;
-
-      const timeSystem::TimeSystem * m_system;
-      timeSystem::AbsoluteTime m_since;
-      timeSystem::AbsoluteTime m_until;
-      timeSystem::AbsoluteTime m_epoch;
-      // TODO: Remove the unit time, or else move it into subclasses, depending on how the design shakes out.
-      // For now it is harmless but unneccessary.
-      timeSystem::Duration m_unit_time;
   };
 
   inline st_stream::OStream & operator <<(st_stream::OStream & os, const PulsarEph & eph) { return eph.write(os); }
@@ -105,13 +90,19 @@ namespace pulsarDb {
       */
       FrequencyEph(const std::string & time_system_name, const timeSystem::AbsoluteTime & valid_since,
         const timeSystem::AbsoluteTime & valid_until, const timeSystem::AbsoluteTime & epoch, double ra, double dec,
-        double phi0, double f0, double f1, double f2): PulsarEph(time_system_name, valid_since, valid_until, epoch),
-          m_ra(ra), m_dec(dec), m_phi0(phi0), m_f0(f0), m_f1(f1), m_f2(f2) {}
-
-      FrequencyEph(const FrequencyEph & eph): PulsarEph(eph), m_ra(eph.m_ra), m_dec(eph.m_dec), m_phi0(eph.m_phi0),
-        m_f0(eph.m_f0), m_f1(eph.m_f1), m_f2(eph.m_f2) {}
+        double phi0, double f0, double f1, double f2): m_system(&timeSystem::TimeSystem::getSystem(time_system_name)),
+        m_since(valid_since), m_until(valid_until), m_epoch(epoch), m_unit_time(0, 1.), m_ra(ra), m_dec(dec), m_phi0(phi0),
+        m_f0(f0), m_f1(f1), m_f2(f2) {}
 
       virtual ~FrequencyEph() {}
+
+      virtual const timeSystem::TimeSystem & getSystem() const { return *m_system; }
+
+      virtual const timeSystem::AbsoluteTime & getValidSince() const { return m_since; }
+
+      virtual const timeSystem::AbsoluteTime & getValidUntil() const { return m_until; }
+
+      virtual const timeSystem::AbsoluteTime & getEpoch() const { return m_epoch; }
 
       virtual PulsarEph * clone() const { return new FrequencyEph(*this); }
 
@@ -134,6 +125,17 @@ namespace pulsarDb {
       double f2() const { return m_f2; }
 
     private:
+      // TODO: Rename dt method to calcElapsedSecond? Or computeElapsedSecond like in PulsarToolApp?
+      virtual double dt(const timeSystem::AbsoluteTime & at1, const timeSystem::AbsoluteTime & at2) const;
+      virtual double dt(const timeSystem::AbsoluteTime & at) const { return dt(at, m_epoch); }
+
+      const timeSystem::TimeSystem * m_system;
+      timeSystem::AbsoluteTime m_since;
+      timeSystem::AbsoluteTime m_until;
+      timeSystem::AbsoluteTime m_epoch;
+      // TODO: Remove the unit time, or else move it into subclasses, depending on how the design shakes out.
+      // For now it is harmless but unneccessary.
+      timeSystem::Duration m_unit_time;
       double m_ra;
       double m_dec;
       double m_phi0;
@@ -156,13 +158,19 @@ namespace pulsarDb {
       */
       PeriodEph(const std::string & time_system_name, const timeSystem::AbsoluteTime & valid_since,
         const timeSystem::AbsoluteTime & valid_until, const timeSystem::AbsoluteTime & epoch, double ra, double dec,
-        double phi0, double p0, double p1, double p2): PulsarEph(time_system_name, valid_since, valid_until, epoch),
-        m_ra(ra), m_dec(dec), m_phi0(phi0), m_p0(p0), m_p1(p1), m_p2(p2) {}
-
-      PeriodEph(const PeriodEph & eph): PulsarEph(eph), m_ra(eph.m_ra), m_dec(eph.m_dec), m_phi0(eph.m_phi0),
-        m_p0(eph.m_p0), m_p1(eph.m_p1), m_p2(eph.m_p2) {}
+        double phi0, double p0, double p1, double p2): m_system(&timeSystem::TimeSystem::getSystem(time_system_name)),
+        m_since(valid_since), m_until(valid_until), m_epoch(epoch), m_unit_time(0, 1.), m_ra(ra), m_dec(dec), m_phi0(phi0),
+        m_p0(p0), m_p1(p1), m_p2(p2) {}
 
       virtual ~PeriodEph() {}
+
+      virtual const timeSystem::TimeSystem & getSystem() const { return *m_system; }
+
+      virtual const timeSystem::AbsoluteTime & getValidSince() const { return m_since; }
+
+      virtual const timeSystem::AbsoluteTime & getValidUntil() const { return m_until; }
+
+      virtual const timeSystem::AbsoluteTime & getEpoch() const { return m_epoch; }
 
       virtual PulsarEph * clone() const { return new PeriodEph(*this); }
 
@@ -185,6 +193,17 @@ namespace pulsarDb {
       double f2() const { double p0sq = m_p0 * m_p0; return 2. * m_p1 * m_p1 / (m_p0 * p0sq) - m_p2 / p0sq; }
 
     private:
+      // TODO: Rename dt method to calcElapsedSecond? Or computeElapsedSecond like in PulsarToolApp?
+      virtual double dt(const timeSystem::AbsoluteTime & at1, const timeSystem::AbsoluteTime & at2) const;
+      virtual double dt(const timeSystem::AbsoluteTime & at) const { return dt(at, m_epoch); }
+
+      const timeSystem::TimeSystem * m_system;
+      timeSystem::AbsoluteTime m_since;
+      timeSystem::AbsoluteTime m_until;
+      timeSystem::AbsoluteTime m_epoch;
+      // TODO: Remove the unit time, or else move it into subclasses, depending on how the design shakes out.
+      // For now it is harmless but unneccessary.
+      timeSystem::Duration m_unit_time;
       double m_ra;
       double m_dec;
       double m_phi0;
