3ef707504102b634499cc028aa13293cb4797408
hirayama <>
2008-02-22 23:45:07 +0000

    Support multiple extensions for each kind.

--- src/PulsarDb.cxx
+++ src/PulsarDb.cxx
@@ -32,8 +32,8 @@ using namespace tip;
 
 namespace pulsarDb {
 
-  PulsarDb::PulsarDb(const std::string & in_file, bool edit_in_place): m_in_file(in_file), m_summary(), m_table(),
-    m_spin_par_table(0), m_orbital_par_table(0), m_obs_code_table(0), m_psr_name_table(0), m_spin_factory_cont(),
+  PulsarDb::PulsarDb(const std::string & in_file, bool edit_in_place): m_in_file(in_file), m_summary(), m_all_table(),
+    m_spin_par_table(), m_orbital_par_table(), m_obs_code_table(), m_psr_name_table(), m_spin_factory_cont(),
     m_orbital_factory_cont() {
     loadTables(edit_in_place);
   }
@@ -53,15 +53,14 @@ namespace pulsarDb {
     }
     m_spin_factory_cont.clear();
 
-    // TODO: Change Tip so it has reverse_iterator, then use it to delete in the correct order.
-    for (FileSummary::const_iterator itor = m_summary.begin(); itor != m_summary.end(); ++itor) {
-      TableCont::iterator table_itor = m_table.find(itor->getExtId());
-      if (m_table.end() != table_itor) delete table_itor->second;
-    }
+    // Delete all tip::Table objects.
+    for (TableCont::reverse_iterator itor = m_all_table.rbegin(); itor != m_all_table.rend(); ++itor) delete *itor;
   }
 
   void PulsarDb::filter(const std::string & expression) {
-    m_spin_par_table->filterRows(expression);
+    for (TableCont::iterator itor = m_spin_par_table.begin(); itor != m_spin_par_table.end(); ++itor) {
+      (*itor)->filterRows(expression);
+    }
   }
 
   void PulsarDb::filterInterval(double t_start, double t_stop) {
@@ -79,7 +78,9 @@ namespace pulsarDb {
     // This is logically equivalent to:
     // t_stop > VALID_SINCE && VALID_UNTIL > t_start
     os << t_stop << ">VALID_SINCE&&VALID_UNTIL>" << t_start;
-    m_spin_par_table->filterRows(os.str());
+    for (TableCont::iterator itor = m_spin_par_table.begin(); itor != m_spin_par_table.end(); ++itor) {
+      (*itor)->filterRows(os.str());
+    }
     clean();
   }
 
@@ -94,19 +95,23 @@ namespace pulsarDb {
 
     std::set<std::string> name_matches;
     // Look up the given name in the extension containing alternate names.
-    for (Table::Iterator itor = m_psr_name_table->begin(); itor != m_psr_name_table->end(); ++itor) {
-      // See if there is an alternate name.
-      std::string alt_name;
-      (*itor)["ALTNAME"].get(alt_name);
+    for (TableCont::iterator table_itor = m_psr_name_table.begin(); table_itor != m_psr_name_table.end(); ++table_itor) {
+      const Table & name_table = **table_itor;
+      for (Table::ConstIterator record_itor = name_table.begin(); record_itor != name_table.end(); ++record_itor) {
+        Table::ConstRecord & record(*record_itor);
+        // See if there is an alternate name.
+        std::string alt_name;
+        record["ALTNAME"].get(alt_name);
 
-      // Convert to uppercase.
-      for (std::string::iterator s_itor = alt_name.begin(); s_itor != alt_name.end(); ++s_itor) *s_itor = toupper(*s_itor);
+        // Convert to uppercase.
+        for (std::string::iterator s_itor = alt_name.begin(); s_itor != alt_name.end(); ++s_itor) *s_itor = toupper(*s_itor);
 
-      if (alt_name == up_name) {
-        // Found a match.
-        std::string real_name;
-        (*itor)["PSRNAME"].get(real_name);
-        name_matches.insert(real_name);
+        if (alt_name == up_name) {
+          // Found a match.
+          std::string real_name;
+          record["PSRNAME"].get(real_name);
+          name_matches.insert(real_name);
+        }
       }
     }
 
@@ -121,8 +126,12 @@ namespace pulsarDb {
     std::string expression = createFilter("PSRNAME", name_matches);
 
     // OK, do the filter for real on the spin parameters and orbital parameters tables.
-    m_spin_par_table->filterRows(expression);
-    m_orbital_par_table->filterRows(expression);
+    for (TableCont::iterator itor = m_spin_par_table.begin(); itor != m_spin_par_table.end(); ++itor) {
+      (*itor)->filterRows(expression);
+    }
+    for (TableCont::iterator itor = m_orbital_par_table.begin(); itor != m_orbital_par_table.end(); ++itor) {
+      (*itor)->filterRows(expression);
+    }
     clean();
   }
 
@@ -146,8 +155,12 @@ namespace pulsarDb {
     std::string expression = createFilter("SOLAR_SYSTEM_EPHEMERIS", name_matches);
 
     // OK, do the filter for real on the spin parameters and orbital parameters tables.
-    m_spin_par_table->filterRows(expression);
-    m_orbital_par_table->filterRows(expression);
+    for (TableCont::iterator itor = m_spin_par_table.begin(); itor != m_spin_par_table.end(); ++itor) {
+      (*itor)->filterRows(expression);
+    }
+    for (TableCont::iterator itor = m_orbital_par_table.begin(); itor != m_orbital_par_table.end(); ++itor) {
+      (*itor)->filterRows(expression);
+    }
     clean();
   }
 
@@ -177,16 +190,26 @@ namespace pulsarDb {
 
     // Loop over all extensions in input file.
     // Skip the primary by incrementing the iterator in the first clause of the for loop.
+    // TODO: Revise this section for new design of D4 FITS definition.
     for (++ext_itor; ext_itor != m_summary.end(); ++ext_itor) {
-      TableCont::const_iterator table_itor = m_table.find(ext_itor->getExtId());
-      if (m_table.end() == table_itor)
-        throw std::logic_error("Could not find extension \"" + ext_itor->getExtId() + "\" in file \"" + m_in_file + "\"");
+      const std::string ext_name = ext_itor->getExtId();
 
-      // Input table pointer.
-      const Table * in_table = table_itor->second;
+      // Find the right tip::Table object.
+      const TableCont * table_cont(0);
+      const Table * in_table(0);
+      if ("SPIN_PARAMETERS" == ext_name) table_cont = &m_spin_par_table;
+      else if ("ORBITAL_PARAMETERS" == ext_name) table_cont = &m_orbital_par_table;
+      else if ("OBSERVERS" == ext_name) table_cont = &m_obs_code_table;
+      else if ("ALTERNATIVE_NAMES" == ext_name) table_cont = &m_psr_name_table;
+      else throw std::runtime_error("Unknown extension name: " + ext_name);
+      if (table_cont->empty()) {
+        throw std::logic_error("Could not find extension \"" + ext_name + "\" in file \"" + m_in_file + "\"");
+      } else {
+        in_table = *(table_cont->begin());
+      }
 
       // Open output table.
-      std::auto_ptr<Table> out_table(file_svc.editTable(out_file, ext_itor->getExtId()));
+      std::auto_ptr<Table> out_table(file_svc.editTable(out_file, ext_name));
 
       // Update standard keywords.
       updateKeywords(*out_table);
@@ -221,36 +244,45 @@ namespace pulsarDb {
 
     // Compose a set of all pulsar names and observer codes in spin extension. This is used below
     // to filter the other extensions in the file.
-    for (Table::Iterator in_itor = m_spin_par_table->begin(); in_itor != m_spin_par_table->end(); ++in_itor) {
-      std::string tmp;
+    for (TableCont::iterator table_itor = m_spin_par_table.begin(); table_itor != m_spin_par_table.end(); ++table_itor) {
+      const Table & spin_table = **table_itor;
+      for (Table::ConstIterator in_itor = spin_table.begin(); in_itor != spin_table.end(); ++in_itor) {
+        std::string tmp;
 
-      (*in_itor)["PSRNAME"].get(tmp);
-      pulsar_names.insert(tmp);
+        (*in_itor)["PSRNAME"].get(tmp);
+        pulsar_names.insert(tmp);
 
-      (*in_itor)["OBSERVER_CODE"].get(tmp);
-      observer_codes.insert(tmp);
+        (*in_itor)["OBSERVER_CODE"].get(tmp);
+        observer_codes.insert(tmp);
+      }
     }
 
     // Compose a set of all pulsar names and observer codes in orbital extension. This is used below
     // to filter the other extensions in the file.
-    for (Table::Iterator in_itor = m_orbital_par_table->begin(); in_itor != m_orbital_par_table->end(); ++in_itor) {
-      std::string tmp;
+    for (TableCont::iterator table_itor = m_orbital_par_table.begin(); table_itor != m_orbital_par_table.end(); ++table_itor) {
+      const Table & orbital_table = **table_itor;
+      for (Table::ConstIterator in_itor = orbital_table.begin(); in_itor != orbital_table.end(); ++in_itor) {
+        std::string tmp;
 
-      (*in_itor)["PSRNAME"].get(tmp);
-      pulsar_names.insert(tmp);
+        (*in_itor)["PSRNAME"].get(tmp);
+        pulsar_names.insert(tmp);
 
-      (*in_itor)["OBSERVER_CODE"].get(tmp);
-      observer_codes.insert(tmp);
+        (*in_itor)["OBSERVER_CODE"].get(tmp);
+        observer_codes.insert(tmp);
+      }
     }
 
     // Filter observer codes based on codes found in the spin/orbital extensions.
     std::string expression = createFilter("OBSERVER_CODE", observer_codes);
-    m_obs_code_table->filterRows(expression);
+    for (TableCont::iterator itor = m_obs_code_table.begin(); itor != m_obs_code_table.end(); ++itor) {
+      (*itor)->filterRows(expression);
+    }
 
     // Filter alternative names based on names found in the spin/orbital extensions.
     expression = createFilter("PSRNAME", pulsar_names);
-    m_psr_name_table->filterRows(expression);
-
+    for (TableCont::iterator itor = m_psr_name_table.begin(); itor != m_psr_name_table.end(); ++itor) {
+      (*itor)->filterRows(expression);
+    }
   }
 
   std::string PulsarDb::createFilter(const std::string & field_name, const std::set<std::string> & values) const {
@@ -267,36 +299,45 @@ namespace pulsarDb {
     // Refill this container.
     cont.clear();
 
-    cont.reserve(m_spin_par_table->getNumRecords());
-    const tip::Header & header(m_spin_par_table->getHeader());
-
-    // Try to read EPHSTYLE keyword to select a proper ephemeris factory.
-    const IEphFactory<PulsarEph> * factory(0);
-    std::string eph_style;
-    try {
-      header["EPHSTYLE"].get(eph_style);
-    } catch (const tip::TipException &) {
-      // Use FrequencyEph if EPHSTYLE keyword is missing.
-      factory = &EphFactory<PulsarEph, FrequencyEph>::getFactory();
+    // Reserve space for all ephemerides.
+    int num_record = 0;
+    for (TableCont::const_iterator itor = m_spin_par_table.begin(); itor != m_spin_par_table.end(); ++itor) {
+      num_record += (*itor)->getNumRecords();
     }
+    cont.reserve(num_record);
 
-    // Use a registered subclass of OrbitalEph, if EPHSTYLE keyword exists.
-    if (0 == factory) {
-      spin_factory_cont_type::const_iterator itor = m_spin_factory_cont.find(eph_style);
-      if (itor != m_spin_factory_cont.end()) {
-        factory = itor->second;
-      } else {
-        throw std::runtime_error("Unknown pulsar ephemeris style: EPHSTYLE = " + eph_style);
+    for (TableCont::const_iterator table_itor = m_spin_par_table.begin(); table_itor != m_spin_par_table.end(); ++table_itor) {
+      const Table & spin_table = **table_itor;
+      const tip::Header & header(spin_table.getHeader());
+
+      // Try to read EPHSTYLE keyword to select a proper ephemeris factory.
+      const IEphFactory<PulsarEph> * factory(0);
+      std::string eph_style;
+      try {
+        header["EPHSTYLE"].get(eph_style);
+      } catch (const tip::TipException &) {
+        // Use FrequencyEph if EPHSTYLE keyword is missing.
+        factory = &EphFactory<PulsarEph, FrequencyEph>::getFactory();
       }
-    }
 
-    // Iterate over current selection.
-    for (Table::Iterator itor = m_spin_par_table->begin(); itor != m_spin_par_table->end(); ++itor) {
-      // For convenience, get record from iterator.
-      Table::ConstRecord & record(*itor);
+      // Use a registered subclass of OrbitalEph, if EPHSTYLE keyword exists.
+      if (0 == factory) {
+        spin_factory_cont_type::const_iterator factory_itor = m_spin_factory_cont.find(eph_style);
+        if (factory_itor != m_spin_factory_cont.end()) {
+          factory = factory_itor->second;
+        } else {
+          throw std::runtime_error("Unknown pulsar ephemeris style: EPHSTYLE = " + eph_style);
+        }
+      }
 
-      // Add the ephemeris to the container.
-      cont.push_back(factory->create(record, header));
+      // Iterate over current selection.
+      for (Table::ConstIterator record_itor = spin_table.begin(); record_itor != spin_table.end(); ++record_itor) {
+        // For convenience, get record from iterator.
+        Table::ConstRecord & record(*record_itor);
+
+        // Add the ephemeris to the container.
+        cont.push_back(factory->create(record, header));
+      }
     }
   }
 
@@ -304,9 +345,10 @@ namespace pulsarDb {
     // Empty container then refill it.
     cont.clear();
 
-    if (0 != m_orbital_par_table) {
-      cont.reserve(m_orbital_par_table->getNumRecords());
-      const tip::Header & header(m_orbital_par_table->getHeader());
+    for (TableCont::const_iterator table_itor = m_orbital_par_table.begin(); table_itor != m_orbital_par_table.end(); ++table_itor) {
+      const Table & orbital_table = **table_itor;
+      cont.reserve(orbital_table.getNumRecords());
+      const tip::Header & header(orbital_table.getHeader());
 
       // Try to read EPHSTYLE keyword to select a proper ephemeris factory.
       const IEphFactory<OrbitalEph> * factory(0);
@@ -320,17 +362,17 @@ namespace pulsarDb {
 
       // Use a registered subclass of OrbitalEph, if EPHSTYLE keyword exists.
       if (0 == factory) {
-        orbital_factory_cont_type::const_iterator itor = m_orbital_factory_cont.find(eph_style);
-        if (itor != m_orbital_factory_cont.end()) {
-          factory = itor->second;
+        orbital_factory_cont_type::const_iterator factory_itor = m_orbital_factory_cont.find(eph_style);
+        if (factory_itor != m_orbital_factory_cont.end()) {
+          factory = factory_itor->second;
         } else {
           throw std::runtime_error("Unknown orbital ephemeris style: EPHSTYLE = " + eph_style);
         }
       }
 
-      for (Table::Iterator itor = m_orbital_par_table->begin(); itor != m_orbital_par_table->end(); ++itor) {
+      for (Table::ConstIterator record_itor = orbital_table.begin(); record_itor != orbital_table.end(); ++record_itor) {
         // For convenience, get record from iterator.
-        Table::ConstRecord & record(*itor);
+        Table::ConstRecord & record(*record_itor);
 
         // Add the ephemeris to the container.
         cont.push_back(factory->create(record, header));
@@ -341,19 +383,20 @@ namespace pulsarDb {
   int PulsarDb::getNumEph(bool spin_table) const {
     int num_eph = 0;
 
+    // Get the right table container.
+    const TableCont & table_cont = (spin_table ? m_spin_par_table : m_orbital_par_table);
+
     // Get the number of entry in the requested table.
-    if (spin_table) {
-      num_eph = m_spin_par_table->getNumRecords();
-    } else {
-      num_eph = m_orbital_par_table->getNumRecords();
+    for (TableCont::const_iterator itor = table_cont.begin(); itor != table_cont.end(); ++itor) {
+      num_eph += (*itor)->getNumRecords();
     }
 
     // Return the number of entry in the requested table.
     return num_eph;
   }
 
-  PulsarDb::PulsarDb(): m_in_file(), m_summary(), m_table(), m_spin_par_table(0), m_orbital_par_table(0), m_obs_code_table(0),
-    m_psr_name_table(0) {
+  PulsarDb::PulsarDb(): m_in_file(), m_summary(), m_all_table(), m_spin_par_table(), m_orbital_par_table(), m_obs_code_table(),
+    m_psr_name_table() {
   }
 
   void PulsarDb::loadTables(bool edit_in_place) {
@@ -368,6 +411,7 @@ namespace pulsarDb {
 
     // Skip the primary by incrementing the iterator in the first clause of the for loop.
     for (++ext_itor; ext_itor != m_summary.end(); ++ext_itor) {
+      // Get a table and its extension name.
       Table * table = 0;
       std::string ext_name = ext_itor->getExtId();
       if (edit_in_place) {
@@ -375,28 +419,26 @@ namespace pulsarDb {
       } else {
         table = file_svc.editTable(m_in_file, ext_name, "#row>0");
       }
-      m_table.insert(std::make_pair(ext_name, table));
-    }
 
-    // Find spin parameter table.
-    TableCont::iterator itor = m_table.find("SPIN_PARAMETERS");
-    if (m_table.end() == itor) throw std::runtime_error("Could not find SPIN_PARAMETERS table in file " + m_in_file);
-    m_spin_par_table = itor->second;
-    
-    // Find orbital parameter table.
-    itor = m_table.find("ORBITAL_PARAMETERS");
-    if (m_table.end() == itor) throw std::runtime_error("Could not find ORBITAL_PARAMETERS table in file " + m_in_file);
-    m_orbital_par_table = itor->second;
-    
-    // Find observer codes.
-    itor = m_table.find("OBSERVERS");
-    if (m_table.end() == itor) throw std::runtime_error("Could not find OBSERVERS table in file " + m_in_file);
-    m_obs_code_table = itor->second;
-    
-    // Find pulsar name table.
-    itor = m_table.find("ALTERNATIVE_NAMES");
-    if (m_table.end() == itor) throw std::runtime_error("Could not find ALTERNATIVE_NAMES table in file " + m_in_file);
-    m_psr_name_table = itor->second;
+      // Add this table to an appropriate list of tables.
+      if ("SPIN_PARAMETERS" == ext_name) {
+        m_spin_par_table.push_back(table);
+        m_all_table.push_back(table);
+      } else if ("ORBITAL_PARAMETERS" == ext_name) {
+        m_orbital_par_table.push_back(table);
+        m_all_table.push_back(table);
+      } else if ("OBSERVERS" == ext_name) {
+        m_obs_code_table.push_back(table);
+        m_all_table.push_back(table);
+      } else if ("ALTERNATIVE_NAMES" == ext_name) {
+        m_psr_name_table.push_back(table);
+        m_all_table.push_back(table);
+      } else {
+        // Delete unknown tables.
+        // TODO: Is it OK for gtpulsardb to filter out unknown table(s)?
+        delete table;
+      }
+    }
   }
 
 }
