f4d88f3db291835db19b5642d0b4d1d9348bd181
glastrm <>
2008-02-26 05:27:32 +0000

    Set lib dependencies up for when building libraries

--- pulsarDbLib.py
+++ pulsarDbLib.py
@@ -1,5 +1,7 @@
+#$Id$
 def generate(env, **kw):
-    env.Tool('addLibrary', library = ['pulsarDb'], package = 'pulsarDb')
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library = ['pulsarDb'])
     env.Tool('st_appLib')
     env.Tool('st_facilitiesLib')
     env.Tool('st_streamLib')
