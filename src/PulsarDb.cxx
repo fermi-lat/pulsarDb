538ed5094195170c079c38860a1fecf974c20344
hirayama <>
2008-03-16 02:49:28 +0000

    Change to always hold ephemerides info on memory.

--- src/PulsarDb.cxx
+++ src/PulsarDb.cxx
@@ -9,6 +9,7 @@
 #include <ctime>
 #include <cctype>
 #include <cstddef>
+#include <fstream>
 #include <memory>
 #include <sstream>
 #include <stdexcept>
@@ -32,10 +33,87 @@ using namespace tip;
 
 namespace pulsarDb {
 
-  PulsarDb::PulsarDb(const std::string & in_file, bool edit_in_place): m_in_file(in_file), m_summary(), m_all_table(),
+  PulsarDb::PulsarDb(const std::string & tpl_file): m_tpl_file(tpl_file), m_tip_file(), m_summary(), m_all_table(),
     m_spin_par_table(), m_orbital_par_table(), m_obs_code_table(), m_psr_name_table(), m_spin_factory_cont(),
     m_orbital_factory_cont() {
-    loadTables(edit_in_place);
+    // Get alias to tip's file service, for brevity.
+    IFileSvc & file_svc(IFileSvc::instance());
+
+    // Create a FITS file in memory that holds ephemeris data.
+    m_tip_file = file_svc.createMemFile("pulsardb.fits", m_tpl_file);
+
+    // Get summary of extensions in the memory FITS file.
+    file_svc.getFileSummary(m_tip_file.getName(), m_summary);
+
+    // If memory FITS file is empty, throw exception.
+    FileSummary::const_iterator ext_itor = m_summary.begin();
+    if (m_summary.end() == ext_itor) throw std::runtime_error("Given FITS template produces an empty FITS file");
+
+    // Loop over remaining extensions in output file.
+    // Skip the primary by incrementing the iterator in the first clause of the for loop.
+    int ext_number = 1;
+    for (++ext_itor; ext_itor != m_summary.end(); ++ext_itor, ++ext_number) {
+      // Get a table and its extension name.
+      Table * table = 0;
+      std::ostringstream oss;
+      oss << ext_number;
+      table = file_svc.editTable(m_tip_file.getName(), oss.str());
+
+      // Add this table to an appropriate list of tables.
+      std::string ext_name = ext_itor->getExtId();
+      if ("SPIN_PARAMETERS" == ext_name) {
+        m_spin_par_table.push_back(table);
+        m_all_table.push_back(table);
+      } else if ("ORBITAL_PARAMETERS" == ext_name) {
+        m_orbital_par_table.push_back(table);
+        m_all_table.push_back(table);
+      } else if ("OBSERVERS" == ext_name) {
+        m_obs_code_table.push_back(table);
+        m_all_table.push_back(table);
+      } else if ("ALTERNATIVE_NAMES" == ext_name) {
+        m_psr_name_table.push_back(table);
+        m_all_table.push_back(table);
+      } else {
+        // Do nothing for other extensions.
+        ;
+      }
+    }
+  }
+
+  void PulsarDb::load(const std::string & in_file) {
+    // Create a temporary PulsarDb object in the same format as this object.
+    std::auto_ptr<PulsarDb> pulsar_db(new PulsarDb(m_tpl_file));
+
+    // First try opening a tip file containing the ephemerides.
+    bool try_text = false;
+    try {
+      pulsar_db->loadFits(in_file);
+    } catch (const tip::TipException &) {
+      // Discard the temporary PulsarDb object because it might be corrupted.
+      pulsar_db.reset(0);
+
+      // Hope that maybe it is a text file.
+      try_text = true;
+    }
+
+    // Next try opening it as a text file if there was a problem.
+    if (try_text) {
+      pulsar_db.reset(new PulsarDb(m_tpl_file));
+      try {
+        pulsar_db->loadText(in_file);
+      } catch (const std::exception &) {
+        throw std::runtime_error("Could not open file " + in_file + " as either a FITS file or a text data file");
+      }
+    }
+
+    // Merge the temporary PulsarDb object into this object.
+    loadFits(pulsar_db->m_tip_file.getName());
+
+    // Refresh the tip table objects.
+    // TODO: Do we really need to do this?
+    for (TableCont::iterator itor = m_all_table.begin(); itor != m_all_table.end(); ++itor) {
+      (*itor)->filterRows("#row>0");
+    }
   }
 
   PulsarDb::~PulsarDb() {
@@ -164,7 +242,7 @@ namespace pulsarDb {
     clean();
   }
 
-  void PulsarDb::save(const std::string & out_file, const std::string & tpl_file) const {
+  void PulsarDb::save(const std::string & out_file, bool clobber) const {
     // Get first extension in input file.
     FileSummary::const_iterator ext_itor = m_summary.begin();
 
@@ -177,10 +255,8 @@ namespace pulsarDb {
     // Find data directory for this app.
     std::string data_dir = facilities::commonUtilities::getDataPath("pulsarDb");
 
-    if (!file_svc.fileExists(out_file)) {
-      // Create output file.
-      file_svc.createFile(out_file, tpl_file);
-    }
+    // Create output file using the same FITS template as this object, respecting clobber.
+    file_svc.createFile(out_file, m_tpl_file, clobber);
 
     // Update keywords only in primary extension.
     std::auto_ptr<Extension> primary(file_svc.editExtension(out_file, ext_itor->getExtId()));
@@ -203,7 +279,7 @@ namespace pulsarDb {
       else if ("ALTERNATIVE_NAMES" == ext_name) table_cont = &m_psr_name_table;
       else throw std::runtime_error("Unknown extension name: " + ext_name);
       if (table_cont->empty()) {
-        throw std::logic_error("Could not find extension \"" + ext_name + "\" in file \"" + m_in_file + "\"");
+        throw std::logic_error("Extension \"" + ext_name + "\" does not exist in pulsar ephemerides file");
       } else {
         in_table = *(table_cont->begin());
       }
@@ -395,50 +471,210 @@ namespace pulsarDb {
     return num_eph;
   }
 
-  PulsarDb::PulsarDb(): m_in_file(), m_summary(), m_all_table(), m_spin_par_table(), m_orbital_par_table(), m_obs_code_table(),
-    m_psr_name_table() {
-  }
-
-  void PulsarDb::loadTables(bool edit_in_place) {
-    // Alias for file service singleton.
+  void PulsarDb::loadFits(const std::string & in_file) {
+    // Get alias to tip's file service, for brevity.
     IFileSvc & file_svc(IFileSvc::instance());
 
     // Get summary of extensions in input file.
-    file_svc.getFileSummary(m_in_file, m_summary);
+    tip::FileSummary in_summary;
+    file_svc.getFileSummary(in_file, in_summary);
 
-    // Loop over remaining extensions in output file.
-    FileSummary::const_iterator ext_itor = m_summary.begin();
+    // Get first extension in input file.
+    FileSummary::const_iterator ext_itor = in_summary.begin();
 
+    // If input file was empty, do nothing.
+    if (in_summary.end() == ext_itor) return;
+
+    // Loop over all extensions in input file.
     // Skip the primary by incrementing the iterator in the first clause of the for loop.
-    for (++ext_itor; ext_itor != m_summary.end(); ++ext_itor) {
-      // Get a table and its extension name.
-      Table * table = 0;
-      std::string ext_name = ext_itor->getExtId();
-      if (edit_in_place) {
-        table = file_svc.editTable(m_in_file, ext_name);
+    int ext_number = 1;
+    for (++ext_itor; ext_itor != in_summary.end(); ++ext_itor, ++ext_number) {
+      const std::string ext_name = ext_itor->getExtId();
+
+      // Find the right tip::Table object.
+      // TODO: Generalize this part for new D4 FITS definition with multiple ephemerides models.
+      TableCont * target_table_cont(0);
+      Table * target_table(0);
+      if ("SPIN_PARAMETERS" == ext_name) target_table_cont = &m_spin_par_table;
+      else if ("ORBITAL_PARAMETERS" == ext_name) target_table_cont = &m_orbital_par_table;
+      else if ("OBSERVERS" == ext_name) target_table_cont = &m_obs_code_table;
+      else if ("ALTERNATIVE_NAMES" == ext_name) target_table_cont = &m_psr_name_table;
+      else throw std::runtime_error("Unknown extension name: " + ext_name);
+      if (target_table_cont->empty()) {
+        throw std::logic_error("Could not find extension \"" + ext_name + "\" in file \"" + in_file + "\"");
       } else {
-        table = file_svc.editTable(m_in_file, ext_name, "#row>0");
+        target_table = *(target_table_cont->begin());
       }
 
-      // Add this table to an appropriate list of tables.
-      if ("SPIN_PARAMETERS" == ext_name) {
-        m_spin_par_table.push_back(table);
-        m_all_table.push_back(table);
-      } else if ("ORBITAL_PARAMETERS" == ext_name) {
-        m_orbital_par_table.push_back(table);
-        m_all_table.push_back(table);
-      } else if ("OBSERVERS" == ext_name) {
-        m_obs_code_table.push_back(table);
-        m_all_table.push_back(table);
-      } else if ("ALTERNATIVE_NAMES" == ext_name) {
-        m_psr_name_table.push_back(table);
-        m_all_table.push_back(table);
-      } else {
-        // Delete unknown tables.
-        // TODO: Is it OK for gtpulsardb to filter out unknown table(s)?
-        delete table;
-      }
+      // Open input table by extension number, in order to work with multiple extensions of the same extension name.
+      std::ostringstream oss;
+      oss << ext_number;
+      std::auto_ptr<const Table> in_table(file_svc.readTable(in_file, oss.str()));
+
+      // Start at beginning of both tables.
+      Table::ConstIterator in_itor = in_table->begin();
+      Table::Iterator target_itor = target_table->end();
+
+      // TODO: Resize output to match input. Requires tip to handle random access iterators.
+      // target_table->setNumRecords(in_table->getNumRecords() + target_table->getNumRecords());
+
+      // Copy all rows in table.
+      for (; in_itor != in_table->end(); ++in_itor, ++target_itor) *target_itor = *in_itor;
     }
   }
 
+  void PulsarDb::loadText(const std::string & in_file) {
+    // Open input text file.
+    std::ifstream in_table(in_file.c_str());
+
+    // Make sure it worked.
+    if (!in_table) throw std::runtime_error("Could not open file " + in_file);
+
+    // Create a text buffer.
+    static const size_t s_line_size = 2048;
+    char buf[s_line_size];
+
+    std::auto_ptr<Table> out_table(0);
+    ParsedLine parsed_line;
+    ParsedLine field_name;
+
+    // Read table until extension name is found.
+    std::string extension;
+    while (in_table) {
+      // Get next line.
+      in_table.getline(buf, s_line_size);
+
+      // Parse it into fields.
+      parseLine(buf, parsed_line);
+
+      if (parsed_line.size() > 1) throw std::runtime_error(std::string("Expected name of an extension on line ") + buf);
+      else if (parsed_line.size() == 1) {
+        extension = *parsed_line.begin();
+        out_table.reset(m_tip_file.editTable(extension));
+        // Get list of fields in the table.
+        Table::FieldCont field = out_table->getValidFields();
+        field_name.assign(field.begin(), field.end());
+        break;
+      }
+    }
+      
+    std::map<std::string, size_t> found_map;
+
+    // Read table until names of fields are found.
+    while (in_table) {
+      // Get next line.
+      in_table.getline(buf, s_line_size);
+
+      // Parse it into fields.
+      parseLine(buf, parsed_line);
+
+      if (parsed_line.size() > 0) {
+        for (ParsedLine::iterator itor = parsed_line.begin(); itor != parsed_line.end(); ++itor) {
+          // Convert to lowercase, because field names are all lowercase.
+          for (std::string::iterator s_itor = itor->begin(); s_itor != itor->end(); ++s_itor) *s_itor = tolower(*s_itor);
+        }
+
+        // Populate input_column number as a function of output column name.
+        for (ParsedLine::iterator itor = field_name.begin(); itor != field_name.end(); ++itor) {
+          std::vector<std::string>::iterator found_itor = std::find(parsed_line.begin(), parsed_line.end(), *itor);
+          if (parsed_line.end() != found_itor) {
+            found_map[*itor] = found_itor - parsed_line.begin();
+          }
+        }
+        break;
+      }
+    }
+  
+    if (found_map.empty())
+      throw std::runtime_error("File " + in_file + " does not have any columns in common with output extension " + extension);
+
+    // Populate output table starting at the beginning.
+    Table::Iterator out_itor = out_table->begin();
+
+    // Read the rest of input table to populate fields.
+    while (in_table) {
+      // Get next line.
+      in_table.getline(buf, s_line_size);
+
+      // Parse it into fields.
+      parseLine(buf, parsed_line);
+
+      if (parsed_line.size() == 0) continue;
+
+      // Make sure the correct number of fields are found.
+      if (parsed_line.size() != found_map.size()) {
+        std::ostringstream os;
+        os << "Line " << buf << " has " << parsed_line.size() << " token(s), not " << found_map.size() << ", as expected";
+        throw std::runtime_error(os.str());
+      }
+  
+      // Populate the parsed line by iterating over output columns.
+      for (ParsedLine::iterator in_itor = field_name.begin(); in_itor != field_name.end(); ++in_itor) {
+        // See if this column was found amongst the input table columns.
+        std::map<std::string, size_t>::iterator found_itor = found_map.find(*in_itor);
+        if (found_map.end() != found_itor) {
+          // Found column, so look up the input table column number from the output column number.
+          (*out_itor)[*in_itor].set(parsed_line[found_itor->second]);
+        } else {
+          // TODO: Make this write an INDEF in the field (requires further changes to tip).
+          // (*out_itor)[*in_itor].set([found_itor->second]);
+        }
+      }
+      ++out_itor;
+    }
+  }
+
+  void PulsarDb::parseLine(const char * line, ParsedLine & parsed_line) {
+    parsed_line.clear();
+
+    const char * begin = line;
+
+    // Skip leading whitespace.
+    while(isspace(*begin)) ++begin;
+
+    // Handle the case of a completely blank line.
+    if ('\0' == *begin) return;
+
+    // Check for comment and return if it is one.
+    if ('#' == *begin) return;
+
+    // At this point there is at least one character, hence one field.
+    parsed_line.resize(1);
+
+    // Start with the first field.
+    std::vector<std::string>::iterator current_field = parsed_line.begin();
+
+    // Start with no quotes.
+    bool in_quote = false;
+    const char * current = begin;
+    while ('\0' != *current) {
+      // Check for quote, skip them but toggle the flag.
+      if ('"' == *current) {
+        in_quote = !in_quote;
+        ++current;
+        continue;
+      }
+
+      // Unquoted space is taken as the end of this field.
+      if (!in_quote && isspace(*current)) {
+        // Add a new field at the end of the container.
+        current_field = parsed_line.insert(parsed_line.end(), "");
+
+        // Skip spaces after this field.
+        while (isspace(*current)) ++current;
+
+        // Start the next field.
+        begin = current;
+        continue;
+      }
+
+      // Add the character to the current field.
+      current_field->push_back(*current);
+
+      // Continue to next character.
+      ++current;
+    }
+
+    if (in_quote) throw std::runtime_error(std::string("Line ") + line + " contains an unbalanced quote");
+  }
 }
