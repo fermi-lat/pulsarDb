93166e5dfcbcdf77e4a733fcdce25e30c34e161e
hirayama <>
2008-06-01 22:24:46 +0000

    Modify to use new Duration interface.

--- src/SimpleDdEph.cxx
+++ src/SimpleDdEph.cxx
@@ -114,7 +114,7 @@ namespace pulsarDb {
   SimpleDdEph::~SimpleDdEph() {}
 
   double SimpleDdEph::calcElapsedSecond(const timeSystem::AbsoluteTime & at) const {
-    return (at - m_t0).computeElapsedTime(m_system->getName()).getTime().getValue(Sec).getDouble();
+    return (at - m_t0).computeElapsedTime(m_system->getName()).getTime().get("Sec");
   }
 
   void SimpleDdEph::writeModelParameter(st_stream::OStream & os) const {
