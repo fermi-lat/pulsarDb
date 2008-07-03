1767033121a432b4da3eadd915e8fae2c87dd12e
hirayama <>
2008-07-03 18:25:50 +0000

    Accept D4 FITS/TEXT file(s) in the original format w/o EPHSTYLE.

--- src/PulsarDbApp.cxx
+++ src/PulsarDbApp.cxx
@@ -80,7 +80,7 @@ namespace pulsarDb {
     if (file_names.empty()) throw std::runtime_error("No files were found matching input file \"" + in_file + "\"");
 
     // Create an empty pulsar ephemerides database, using the template file.
-    PulsarDb data_base(m_tpl_file);
+    PulsarDb data_base(m_tpl_file, 1, 2);
 
     // Load input ephemerides.
     for (st_facilities::FileSys::FileNameCont::const_iterator itor = file_names.begin(); itor != file_names.end(); ++itor) {
