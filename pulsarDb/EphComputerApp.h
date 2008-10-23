ec9439d5628d66f870c37935d9fa788f2196b62f
hirayama <>
2008-10-23 01:11:00 +0000

    Remove unnecessary header file to include. Add/improve Doxygen comments.

--- pulsarDb/EphComputerApp.h
+++ pulsarDb/EphComputerApp.h
@@ -8,8 +8,6 @@
 
 #include "pulsarDb/PulsarToolApp.h"
 
-#include "st_app/StApp.h"
-
 #include "st_stream/StreamFormatter.h"
 
 namespace pulsarDb {
@@ -17,12 +15,17 @@ namespace pulsarDb {
   class EphComputer;
 
   /** \class EphComputerApp
-      \brief Main application class for pulsar database access.
+      \brief Main application class for ephemeris computations using pulsar database.
   */
   class EphComputerApp: public pulsarDb::PulsarToolApp {
     public:
+      /// \brief Construct an EphComputerApp object.
       EphComputerApp();
+
+      /// \brief Destruct this EphComputerApp object.
       virtual ~EphComputerApp() throw();
+
+      /// \brief Run the application.
       virtual void runApp();
 
     private:
