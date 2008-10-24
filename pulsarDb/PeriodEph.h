9b30997b85d193a7a77fa667e143db2cbf4d3aad
hirayama <>
2008-10-24 01:19:11 +0000

    Add/improve Doxygen comments.

--- pulsarDb/PeriodEph.h
+++ pulsarDb/PeriodEph.h
@@ -46,29 +46,67 @@ namespace pulsarDb {
         double phi0, double p0, double p1, double p2): m_system(&timeSystem::TimeSystem::getSystem(time_system_name)),
         m_since(valid_since), m_until(valid_until), m_epoch(epoch), m_ra(ra), m_dec(dec), m_phi0(phi0), m_p0(p0), m_p1(p1), m_p2(p2) {}
 
+      /// \brief Destruct this PeriodEph object.
       virtual ~PeriodEph() {}
 
+      /// \brief Return a time system to be used to interpret return values of calcFrequency method.
       virtual const timeSystem::TimeSystem & getSystem() const { return *m_system; }
 
+      /** \brief Return a start time of a time interval, during which this ephemeris is considered valid.
+                 Note: The ephemeris is also considered valid on the start time itself.
+      */
       virtual const timeSystem::AbsoluteTime & getValidSince() const { return m_since; }
 
+      /** \brief Return an end time of a time interval, during which this ephemeris is considered valid.
+                 Note: The ephemeris is also considered valid on the end time itself.
+      */
       virtual const timeSystem::AbsoluteTime & getValidUntil() const { return m_until; }
 
+      /// \brief Return a reference epoch of this ephemeris.
       virtual const timeSystem::AbsoluteTime & getEpoch() const { return m_epoch; }
 
+      /// \brief Create a copy of this object, and return a pointer to it.
       virtual PulsarEph * clone() const { return new PeriodEph(*this); }
 
+      /** \brief Compute a pulse phase of a given time, and return it.
+                 Note: validity of the ephemeris (valid since and valid until) are not checked.
+          \param ev_time Absolute time for which a pulse phase is to be computed.
+          \param phase_offset Value to be added to a pulse phase. This value is added to the computed pulse phase
+                 before truncated to a value in range [0, 1).
+      */
       virtual double calcPulsePhase(const timeSystem::AbsoluteTime & ev_time, double phase_offset = 0.) const;
 
+      /** \brief Compute a pulse frequency at a given time, and return it.
+                 Note: validity of the ephemeris (valid since and valid until) are not checked.
+          \param ev_time Absolute time at which a pulse frequency is to be computed.
+          \param derivative_order Order of frequency derivative to be computed. Set zero (0) to this argument
+                 to compute a pulse frequency, one (1) the first time derivative of pulse frequency, and so on.
+      */
       virtual double calcFrequency(const timeSystem::AbsoluteTime & ev_time, int derivative_order = 0) const;
 
+      /** \brief Compute a sky position at a given time, and return it. The returned value is a pair of Right Ascension
+                 and Declination of the position.
+                 Note: validity of the ephemeris (valid since and valid until) are not checked.
+          \param ev_time Absolute time at which a sky position is to be computed.
+      */
       virtual std::pair<double, double> calcSkyPosition(const timeSystem::AbsoluteTime & ev_time) const;
 
     protected:
+      /// \brief Output text expression of subclass-specific parameters of this object to a given output stream.
       virtual void writeModelParameter(st_stream::OStream & os) const;
 
     private:
+      /** \brief Compute the number of elapsed seconds between given absolute times in the time system in which this ephemeris
+                 is defined, and return it.
+          \param at1 Absolute time at which the number of elapsed seconds is to be computed.
+          \param at2 Absolute time that gives a start time to count the number of elapsed seconds.
+      */
       virtual double calcElapsedSecond(const timeSystem::AbsoluteTime & at1, const timeSystem::AbsoluteTime & at2) const;
+
+      /** \brief Compute the number of elapsed seconds at a given absolute time in the time system in which this ephemeris
+                 is defined, and return it.
+          \param at Absolute time at which the number of elapsed seconds is to be computed.
+      */
       virtual double calcElapsedSecond(const timeSystem::AbsoluteTime & at) const { return calcElapsedSecond(at, m_epoch); }
 
       const timeSystem::TimeSystem * m_system;
--- pulsarDb/PulsarToolApp.h
+++ pulsarDb/PulsarToolApp.h
@@ -41,6 +41,9 @@ namespace pulsarDb {
   class EphChooser;
   class EphComputer;
 
+  /** \class PulsarToolApp
+      \brief Base class for pulsar tool application classes.
+  */
   class PulsarToolApp : public st_app::StApp {
     public:
       typedef std::vector<timeSystem::EventTimeHandler *> handler_cont_type;
@@ -49,67 +52,174 @@ namespace pulsarDb {
         REQUIRED, ALLOWED, SUPPRESSED
       };
 
+      /// \brief Construct a PulsarToolApp object.
       PulsarToolApp();
+
+      /// \brief Destruct this PulsarToolApp object.
       virtual ~PulsarToolApp() throw();
+
+      /** \brief Run the application. This method calls runApp method after cleaning up the internal data members,
+                 and clean them up again after the call of runApp method.
+      */
       virtual void run();
+
+      /// \brief Run the tool-specific part of the application.
       virtual void runApp() = 0;
 
+      /** \brief Interpret a character string as a time representation in a specified format, create an AbsoluteTime object
+                 that represents the given time string, and return it.
+          \param time_format Character string that specifies a time format (e.g., "MJD"). If this parameter is set to "FILE",
+                 then the same time format is chosen as the opened event file that is listed first.
+          \param time_system Name of time system in which the time string is to be interpreted. If this parameter is set to
+                 "FILE", then the time system name is read from TIMESYS keyword of a given FITS header.
+          \param time_value Character string that represents a time of interest.
+          \param parsed_time_format Name of time format that this method determines to use is to be set as a return value.
+          \param parsed_time_system Name of time system that this method determines to use is to be set as a return value.
+          \param header FITS header to be used in case of time_system is "FILE". Also, when set to zero (0), it throws
+                 an exception for either one of time_format or time_system being "FILE".
+      */
       virtual timeSystem::AbsoluteTime parseTime(const std::string & time_format, const std::string & time_system,
         const std::string & time_value, std::string & parsed_time_format, std::string & parsed_time_system,
         const tip::Header * header = 0) const;
 
+      /** \brief Open event file(s).
+          \param pars Set of parameters from which event file names, etc., are to be read.
+          \param read_only If true, the file(s) are to be opened in a read-only mode, otherwise in a read-write mode.
+      */
       void openEventFile(const st_app::AppParGroup & pars, bool read_only = true);
 
+      /** \brief Search for a specified field name in the opened event file(s), and append a new column if missing.
+          \param field_name Name of FITS column to be searched for.
+          \param field_format Format of the FITS column to be used in case of appending a new column.
+      */
       void reserveOutputField(const std::string & field_name, const std::string & field_format);
 
+      /** \brief Define a set of time corrections and give it a name for future reference.
+          \param mode_name Name of a time correction mode to define.
+          \param tcmode_bary Time correction mode on the arrival time correction.
+          \param tcmode_bin Time correction mode on the binary demodulation.
+          \param tcmode_pdot Time correction mode on the p-dot cancellation.
+      */
       void defineTimeCorrectionMode(const std::string & mode_name, TimeCorrectionMode_e tcmode_bary, TimeCorrectionMode_e tcmode_bin,
         TimeCorrectionMode_e tcmode_pdot);
 
+      /** \brief Select a set of timing modes to use after a call of this method.
+          \param mode_name Name of the time correction mode to select.
+      */
       void selectTimeCorrectionMode(const std::string & mode_name);
 
+      /** \brief Select a set of timing modes to use after a call of this method. The mode name is read from a given set of parameters.
+          \param pars Set of parameters from which a time correction mode is read.
+      */
       void selectTimeCorrectionMode(const st_app::AppParGroup & pars);
 
+      /** \brief Initialize ephemeris computations, with the ephemeris style explicitly specified.
+          \param pars Set of parameters from which initialization information is to be read.
+          \param chooser Ephemeris chooser to be used in the application.
+          \param eph_style Character string that specifies ephemeris style such as "FREQ" or "DB".
+          \param os Output stream to write the ancestry records of pulsar ephemeris database to.
+      */
       void initEphComputer(const st_app::AppParGroup & pars, const EphChooser & chooser, const std::string & eph_style,
         st_stream::OStream & os);
 
+      /** \brief Initialize ephemeris computations, with the ephemeris style to be read from a given set of parameters.
+          \param pars Set of parameters from which initialization information is to be read.
+          \param chooser Ephemeris chooser to be used in the application.
+          \param os Output stream to write the ancestry records of pulsar ephemeris database to.
+      */
       void initEphComputer(const st_app::AppParGroup & pars, const EphChooser & chooser, st_stream::OStream & os);
 
+      /** \brief Initializa arrival time corrections, with the time origin to be read from a given set of parameters.
+          \param pars Set of parameters from which initialization information is to be read.
+          \param vary_ra_dec If true, the sky position of a celestial source is to be read from the stored spin ephemerides
+                 for each event. Otherwise, one position is to be used for all events.
+          \param guess_pdot If true, parameters for p-dot cancellation is to be computed from the stored spin ephemerides.
+                 Otherwise, the method reads them from the given set of parameters.
+      */
       void initTimeCorrection(const st_app::AppParGroup & pars, bool vary_ra_dec, bool guess_pdot);
 
+      /** \brief Initializa arrival time corrections, with the time origin specified by a character string.
+          \param pars Set of parameters from which initialization information is to be read.
+          \param vary_ra_dec If true, the sky position of a celestial source is to be read from the stored spin ephemerides
+                 for each event. Otherwise, one position is to be used for all events.
+          \param guess_pdot If true, parameters for p-dot cancellation is to be computed from the stored spin ephemerides.
+                 Otherwise, the method reads them from the given set of parameters.
+          \param str_origin Character string representing the time origin.
+      */
       void initTimeCorrection(const st_app::AppParGroup & pars, bool vary_ra_dec, bool guess_pdot, const std::string & str_origin);
 
+      /** \brief Initializa arrival time corrections, with the time origin explicitly given.
+          \param pars Set of parameters from which initialization information is to be read.
+          \param vary_ra_dec If true, the sky position of a celestial source is to be read from the stored spin ephemerides
+                 for each event. Otherwise, one position is to be used for all events.
+          \param guess_pdot If true, parameters for p-dot cancellation is to be computed from the stored spin ephemerides.
+                 Otherwise, the method reads them from the given set of parameters.
+          \param abs_origin AbsoluteTime object representing the time origin.
+      */
       void initTimeCorrection(const st_app::AppParGroup & pars, bool vary_ra_dec, bool guess_pdot,
         const timeSystem::AbsoluteTime & abs_origin);
 
+      /** \brief Compute the number of elepased seconds since the time origin.
+          \param abs_time Absolute time for which the number of elapsed seconds is to be computed.
+      */
       double computeElapsedSecond(const timeSystem::AbsoluteTime & abs_time) const;
 
+      /** \brief Create an AbsoluteTime object that represents the time after a given elapsed time since the time origin.
+          \param elapsed_time Number of elapsed seconds since the time origin.
+      */
       timeSystem::AbsoluteTime computeAbsoluteTime(double elapsed_time) const;
 
+      /// \brief Set the internal event iterator to point to the first event in the opened FITS file.
       void setFirstEvent();
 
+      /// \brief Increment the internal event iterator to point to the next event in the opened FITS file.
       void setNextEvent();
 
+      /// \brief Return if the internal event iterator points to one past the last event in the opened FITS file.
       bool isEndOfEventList() const;
 
+      /// \brief Create an AbsoluteTime object that represents the current event time, and return it.
       timeSystem::AbsoluteTime getEventTime() const;
 
+      /** \brief Write a value to a field of the current FITS row.
+          \param field_name Name of the field to be written a given value.
+          \param field_value Value of the field to be written.
+      */
       template <typename DataType>
       void setFieldValue(const std::string & field_name, const DataType & field_value) {
         tip::TableRecord & record = (*m_event_handler_itor)->getCurrentRecord();
         record[field_name].set(field_value);
       }
 
+      /// \brief Create an AbsoluteTime object that represents the earliest time in GTI extension(s).
       timeSystem::AbsoluteTime getStartTime() const;
 
+      /// \brief Create an AbsoluteTime object that represents the latest time in GTI extension(s).
       timeSystem::AbsoluteTime getStopTime() const;
 
+      /// \brief Return a reference to the internal ephemeris computer for external use.
       EphComputer & getEphComputer() const;
 
+      /** \brief Examine ephemeris coverage and ephemeris remarks relevant for the opened event file(s),
+                 and output a summary report of the ephemeris status into a given output stream.
+          \param os Output stream to write the summary report to.
+          \param code_to_report List of ephemeris status code to be reported.
+      */
       void reportEphStatus(st_stream::OStream & os, const std::set<EphStatusCodeType> & code_to_report) const;
 
+      /** \brief Examine ephemeris coverage and ephemeris remarks relevant for a given time interval,
+                 and output a summary report of the ephemeris status into a given output stream.
+          \param os Output stream to write the summary report to.
+          \param start_time Start time of the time interval to examine ephemerides.
+          \param stop_time Stop time of the time interval to examine ephemerides.
+          \param code_to_report List of ephemeris status code to be reported.
+      */
       void reportEphStatus(st_stream::OStream & os, const timeSystem::AbsoluteTime & start_time,
         const timeSystem::AbsoluteTime & stop_time, const std::set<EphStatusCodeType> & code_to_report) const;
 
+      /** \brief Write a list of parameters to the header(s) of the event file(s).
+          \param pars Set of parameters to be written to the event file(s).
+      */
       void writeParameter(const st_app::AppParGroup & pars);
 
     private:
@@ -142,11 +252,24 @@ namespace pulsarDb {
       */
       virtual void resetApp();
 
+      /** \brief Read a time value from a specified FITS column, create an AbosoluteTime object that represents the time,
+                 apply time correctios on it if requested, and return it.
+          \param handler EventTimeHandler object to read a time value from.
+          \param column_name Name of FITS column from which a time value to be read.
+          \param request_time_correction If true, arrival time corrections are to be requested on the time. Otherwise,
+                 a time read from the event file is converted into an AbsoluteTime object without arrival time corrections.
+      */
       timeSystem::AbsoluteTime readTimeColumn(timeSystem::EventTimeHandler & handler, const std::string & column_name,
         bool request_time_correction) const;
 
+      /** \brief Compute a time boundary (start time or stop time) of the opened event file(s), and return it.
+          \param request_start_time If true, start time is computed. Otherwise, stop time is computed.
+          \param request_time_correction If true, arrival time corrections are to be requested on the time. Otherwise,
+                 a time read from the event file is converted into an AbsoluteTime object without arrival time corrections.
+      */
       timeSystem::AbsoluteTime computeTimeBoundary(bool request_start_time, bool request_time_correction) const;
 
+      /// \brief Set up the internal variables when a new FITS table is opened.
       void setupCurrentEventTable();
   };
 
